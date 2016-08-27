5e44b1d4099f9c0992d6ff103e1011dd78957264
Ember Sabsalon <embersabsalon@gmail.com>
2016-08-27 14:57:01 +0800

    Created by Coding Ground Client

---
+++ New Project-20160827/main.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "sys/types.h"
+#include "unistd.h"
+
+#define   MAX_COUNT  200
+
+void  ChildProcess(void);               
+void  ParentProcess(void);  
+
+void  main(void){
+    pid_t  pid;
+    pid = fork();
+    if (pid == 0){
+        int   i;
+        for (i = 1; i <= MAX_COUNT; i++)
+          printf("From child line, line= %d, pid=%d\n", i,getpid());
+
+    }
+    
+    else{
+        int   i;
+        for (i = 1; i <= MAX_COUNT; i++)
+            printf("From parent line, line= %d, pid=%d\n", i,getpid());
+    }
+}
+
+
